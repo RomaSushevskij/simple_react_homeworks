53e844f25e80e062479e46998c81bae11ad9adc4
RomaSushevskij <roma.sushevskij@yandex.ru>
2022-01-16 19:40:34 +0300

    refactoring

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {PATH} from "./AllRoutes";
 import {NavLink} from "react-router-dom";
 import style from './Header.module.css'
 import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
+import {CSSTransition} from "react-transition-group";
 
 function Header() {
+    const [selectMode, setSelectMode] = useState(false);
+
     return (
         <div className={style.level}>
-            <SuperButton>Select level</SuperButton>
-            <ul>
-                <li><NavLink to={PATH.PRE_JUNIOR}>Pre-Junior</NavLink></li>
-                <li><NavLink to={PATH.JUNIOR}>Junior</NavLink></li>
-                <li><NavLink to={PATH.JUNIOR_PLUS}>Junior-Plus</NavLink></li>
-            </ul>
+            <SuperButton callback={() => setSelectMode(!selectMode)}>Select level</SuperButton>
+            <CSSTransition in={selectMode}
+                           classNames={style}
+                           timeout={600}
+                           unmountOnExit
+                           mountOnEnter>
+                <ul>
+                    <li><NavLink to={PATH.PRE_JUNIOR}>Pre-Junior</NavLink></li>
+                    <li><NavLink to={PATH.JUNIOR}>Junior</NavLink></li>
+                    <li><NavLink to={PATH.JUNIOR_PLUS}>Junior-Plus</NavLink></li>
+                </ul>
+            </CSSTransition>
+
         </div>
     )
 }
