8fb5427be20212339eac1db296f0678021b99c1a
RomaSushevskij <roma.sushevskij@yandex.ru>
2022-03-05 12:09:22 +0300

    DZ_11 refactoring

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -5,33 +5,34 @@ import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 
 
-const minDistance = 5;
+
 
 
 function HW11() {
     const [value1, setValue1] = useState<number | number[]>(20)
-    const [value2, setValue2] = useState<number[]>([20, 30])
+    const [value2, setValue2] = useState<number[]>([20, 40])
+    const minDistance = 5;
 
-    const onChangeCallback1 = (event: Event, newValue: number | number[]) => {
-        setValue1(newValue as number);
-        setValue2([newValue as number, value2[1]]);
-    };
-
-    const onChangeCallback2 = (
+    const onChangeCallback = (
         event: Event,
         newValue: number | number[],
         activeThumb: number,
     ) => {
         if (!Array.isArray(newValue)) {
-            return;
+            const value = Math.min(newValue, value2[1] - minDistance)
+            setValue1(value);
+            setValue2([value, value2[1]]);
         }
-
-        if (activeThumb === 0) {
-            setValue2([Math.min(newValue[0], value2[1] - minDistance), value2[1]]);
-            setValue1(Math.min(newValue[0], value2[1] - minDistance));
-        } else {
-            setValue2([value2[0], Math.max(newValue[1], value2[0] + minDistance)]);
-            setValue1(value2[0]);
+        if (Array.isArray(newValue)) {
+            if (activeThumb === 0) {
+                const min = Math.min(newValue[0], value2[1] - minDistance)
+                setValue2([min, value2[1]]);
+                setValue1(min);
+            } else {
+                const max = Math.max(newValue[1], value2[0] + minDistance)
+                setValue2([value2[0], max]);
+                setValue1(value2[0]);
+            }
         }
     };
 
@@ -49,7 +50,7 @@ function HW11() {
                         <Slider value={value1}
                                 aria-label="Default"
                                 valueLabelDisplay="auto"
-                                onChange={onChangeCallback1}/>
+                                onChange={onChangeCallback}/>
                     </Box>
                 </div>
 
@@ -60,7 +61,7 @@ function HW11() {
                     <Box width={300}>
                         <Slider getAriaLabel={() => 'Minimum distance'}
                                 value={value2}
-                                onChange={onChangeCallback2}
+                                onChange={onChangeCallback}
                                 valueLabelDisplay="auto"
                                 disableSwap/>
                     </Box>
